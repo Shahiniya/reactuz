f4317d03219a39d694fa5bd1adf7b320dc459bf1
Shelli <sh.panjieva@gmail.com>
2022-01-26 11:49:30 +0900

    new

---
+++ src/Toshkent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Toshkent extends React.Component{
+    render(){
+        return <h1>Hello Toshkent</h1>
+    }
+}
+
+export {Toshkent};
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Toshkent} from './Toshkent';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Toshkent/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
